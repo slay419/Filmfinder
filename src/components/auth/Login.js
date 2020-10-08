33627ef45a4519508b483d67cb4fe268aa957925
Aidan <aidansch30@gmail.com>
2020-10-08 12:16:54 +1100

    Added basic forms to login and register

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import React from "react";
+import "../../styles/Login.scss";
+import { findAllInRenderedTree } from "react-dom/test-utils";
+import { Link } from "react-router-dom";
+
 
 const Login = () => {
+  const [input, setInput] = useState("");
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
   return (
-    <div>
+    <div className="login">
       <h1>Login Page</h1>
-      <a href="#">LOGIN</a>
+      <div className="Login-inputs">
+        <form>
+          <label for="name">Username:</label>
+          <input type="text" placeholder="Enter username" id="name"></input>
+
+          <label for="pword">Password:</label>
+          <input type="password" placeholder="Enter password" id="pword"></input>
+
+          <button type="button" onClick={()=> alert("bold of you to assume this would work")}>Login</button>
+          <p>Not Registered ?<Link to="/register">Create account Now!</Link></p>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "../../styles/register.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register">
+      <h1>Register Now</h1>
+      <form>
+          <label for="name">Username:</label>
+          <input type="text" placeholder="Enter username" id="name"></input>
+
+          <label for="email">Email:</label>
+          <input type="text" placeholder="Enter email" id="email"></input>
+
+          <label for="pword">Password:</label>
+          <input type="password" placeholder="Enter password" id="pword"></input>
+
+          <button type="button" onClick={()=> alert("bold of you to assume this would work")}>register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
