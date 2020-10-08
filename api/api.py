cd0de028225f47395a4fa1c81caa8dceda5eb2a0
Kevin Chau <kevin.chau5@outlook.com>
2020-10-08 22:22:48 +1100

    updated db and fixed movie filtering

--- api/api.py
+++ api/api.py
@@ -45,19 +45,55 @@ class Movie(Resource):
     def get(self):
         title_str = title_parser.parse_args().get("title") 
         conn = sqlite3.connect("./movieDB.db")
-
-        '''
         cur = conn.cursor()
+        movies = {}
         if title_str is None:
             cur.execute("select * from MOVIE limit 15")
-            return {"movies": df.to_dict("id")}
+            
+        else:
+            cur.execute(
+                f"""
+                create view temp_id as
+                select movie_id from movie where title like "%{title_str}%"
+                union
+                select movie_id from genre where genre like "%{title_str}%"
+                union 
+                select movie_id from movie where overview like "% {title_str} %";
+                """
+            )
+
+            cur.execute("select * from movie m join temp_id t on m.movie_id = t.movie_id limit 15;")
+                #return {"movies": df.to_dict("id")}
+            
+        index = 0
+        for movie in cur.fetchall():
+            print(movie)
+            item = {}
+            item['movie_id'] = movie[0]
+            item['director_id'] = movie[1]
+            item['adult'] = movie[2]
+            item['title'] = movie[3]
+            item['release_date'] = movie[4]
+            item['runtime'] = movie[5]
+            item['budget'] = movie[6]
+            item['revenue'] = movie[7]
+            item['imdb_id'] = movie[8]
+            item['language'] = movie[9]
+            item['overview'] = movie[10]
+            item['tagline'] = movie[11]
+            item['poster'] = movie[12]
+            item['vote_avg'] = movie[13]
+            item['vote_count'] = movie[14]
+            item['genres'] = getGenreList(item['movie_id'])
+            movies[index] = item
+            index += 1
+        cur.execute("drop view temp_id;")
+        return {"movies": movies}
+
+
         '''
-        
         if title_str is None:
             df = sql.read_sql("select * from MOVIE limit 15", conn,)
-            print(df)
-            df['testcol'][0] = "testdata"
-            print(df)
             return {"movies": df.to_dict("id")}
 
         df = sql.read_sql(
@@ -65,9 +101,8 @@ class Movie(Resource):
             conn,
         )
 
-        
-        
-        return {"movies": df.to_dict("id")}
+        #return {"movies": df.to_dict("id")}
+        '''
 
 
 @app.route("/api/movies/<int:id>")
@@ -101,14 +136,7 @@ def getCastByMovieId(movie_id):
 
 @app.route("/api/genres/<int:movie_id>", methods=["POST"])
 def getGenresByMovieId(movie_id):
-    conn = sqlite3.connect("./movieDB.db")
-    cur = conn.cursor()
-    cur.execute(f"select genre from GENRE where movie_id = {movie_id};")
-    genres = []
-    for genre in cur.fetchall():
-        genres.append(genre[0])
-    print(genres)
-    conn.close()
+    genres = getGenreList(movie_id)
     return {"genres": genres}
     #returns {genres: {...}} or {genres: [...]}
 
@@ -123,12 +151,8 @@ def login():
 
     return auth_login(email, password)
 
-<<<<<<< HEAD
-@app.route('/auth/register', methods=['POST'])
-=======
 
 @app.route("/auth/register", methods=["POST"])
->>>>>>> adjusting-to-new-db
 def register():
     email = request.form.get("email")
     password = request.form.get("password")
@@ -142,6 +166,23 @@ def register():
 
     # if valid then return user
     return auth_register(email, password, first_name, last_name)
+
+
+############### Helper Functions #################
+
+def getGenreList(movie_id):
+    conn = sqlite3.connect("./movieDB.db")
+    cur = conn.cursor()
+    cur.execute(f"select genre from GENRE where movie_id = {movie_id};")
+    genres = []
+    for genre in cur.fetchall():
+        genres.append(genre[0])
+    print(genres)
+    conn.close()
+
+    return genres
+
+
 
 
 if __name__ == "__main__":
