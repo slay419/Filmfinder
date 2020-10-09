459394209c57af82bc6905807ff270be615ff08d
Aidan <aidansch30@gmail.com>
2020-10-09 16:01:56 +1100

    register.js adjustments

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -8,14 +8,17 @@ const Register = () => {
     <div className="register">
       <h1>Register Now</h1>
       <form>
-          <label for="name">Username:</label>
-          <input type="text" placeholder="Enter username" id="name"></input>
-
           <label for="email">Email:</label>
           <input type="text" placeholder="Enter email" id="email"></input>
 
           <label for="pword">Password:</label>
           <input type="password" placeholder="Enter password" id="pword"></input>
+
+          <label for="fname">First Name:</label>
+          <input type="text" placeholder="Enter First Name" id="fname"></input>
+
+          <label for="lname">Username:</label>
+          <input type="text" placeholder="Enter Last Name" id="lname"></input>
 
           <button type="button" onClick={()=> alert("bold of you to assume this would work")}>register</button>
       </form>
