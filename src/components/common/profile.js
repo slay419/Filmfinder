aba4b421e3777ece4cb70c74dbed6662dd152071
Aidan <aidansch30@gmail.com>
2020-10-13 22:07:10 +1100

    moving change password to profile page

--- src/components/common/profile.js
+++ src/components/common/profile.js
@@ -2,6 +2,7 @@ import React from "react";
 import LoginContext from "../../context/Auth/LoginContext";
 import { useContext, useState, useEffect } from "react";
 import ProfileContext from "../../context/Profile/ProfileContext";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
     const loginContext = useContext(LoginContext);
@@ -15,6 +16,7 @@ const Profile = () => {
     return(
         <div>
             <h1>{User.first_name}'s Profile:</h1>
+            <Link to="/change">Change Password</Link>
         </div>
     )
 }
