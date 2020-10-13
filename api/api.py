92c28dd6296f697b52a6ea62b7a59fd1a95dc86a
peterphanouvong <peterphanouvong@gmail.com>
2020-10-13 20:51:23 +1100

    pre-merge with kevdog

--- api/api.py
+++ api/api.py
@@ -8,7 +8,21 @@ from pandas.io import sql
 from requests import get
 import re
 
-from functions.auth import auth_login, auth_register
+from functions.auth import (
+    auth_login,
+    auth_register,
+    get_secret_question,
+    get_secret_answer,
+)
+from functions.search import (
+    searchGenre,
+    searchKeyword,
+    searchDirector,
+    extractMovieDetails,
+    getGenreList,
+    getCastList,
+)
+from functions.review import newReview, incrementLikes, editReview
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "you-will-never-guess"
@@ -50,7 +64,7 @@ class Movie(Resource):
         # Change the sql query depending on if a search term was given or not
         if title_str is None:
             cur.execute("select * from MOVIE limit 15")
-        
+
         else:
             # Search through movie titles, overview and genre for matching keywords in that order
             cur.execute(
@@ -64,81 +78,50 @@ class Movie(Resource):
                 """
             )
 
-            cur.execute("select * from movie m join temp_id t on m.movie_id = t.movie_id group by m.movie_id order by t.subquery limit 15;")
-            #return {"movies": df.to_dict("id")}
-            
+            cur.execute(
+                "select * from movie m join temp_id t on m.movie_id = t.movie_id group by m.movie_id order by t.subquery limit 15;"
+            )
+            # return {"movies": df.to_dict("id")}
+
         index = 0
-        # Extract movie information and populate dictionary 
+        # Extract movie information and populate dictionary
         for movie in cur.fetchall():
-            item = {}
-            item["movie_id"] = movie[0]
-            item["director_id"] = movie[1]
-            item["adult"] = movie[2]
-            item["title"] = movie[3]
-            item["release_date"] = movie[4]
-            item["runtime"] = movie[5]
-            item["budget"] = movie[6]
-            item["revenue"] = movie[7]
-            item["imdb_id"] = movie[8]
-            item["language"] = movie[9]
-            item["overview"] = movie[10]
-            item["tagline"] = movie[11]
-            item["poster"] = movie[12]
-            item["vote_avg"] = movie[13]
-            item["vote_count"] = movie[14]
-            item["genres"] = getGenreList(item["movie_id"])
+            item = extractMovieDetails(movie)
             movies[index] = item
             index += 1
 
         cur.execute("drop view IF EXISTS temp_id;")
         return {"movies": movies}
 
-        """
-        if title_str is None:
-            df = sql.read_sql("select * from MOVIE limit 15", conn,)
-            return {"movies": df.to_dict("id")}
-        df = sql.read_sql(
-            "select * from MOVIE m where m.title like '%" + title_str + "%' limit 15",
-            conn,
-        )
-        #return {"movies": df.to_dict("id")}
-        """
-
 
 @app.route("/api/movies/<int:id>")
 def getMovieById(id):
     conn = sqlite3.connect("./movieDB.db")
-    df = sql.read_sql("select * from MOVIE m where m.movie_id = " + str(id), conn,)
-
-    return {"movie": df.set_index("movie_id").to_dict("index")}
-
-
-@app.route("/api/cast/<int:movie_id>", methods=["POST"])
-def getCastByMovieId(movie_id):
-    conn = sqlite3.connect("./movieDB.db")
     cur = conn.cursor()
-    cur.execute(
-        f"""
-        select c.cast_name
-        from cast c join acting a on c.cast_id = a.actor_id
-        where a.movie_id = {movie_id};
-        """
-    )
-    cast_list = []
-    for cast in cur.fetchall():
-        cast_list.append(cast[0])
-    print(cast_list)
+    cur.execute(f"select * from MOVIE where movie_id = {id}")
+    movie = cur.fetchone()
+    item = {}
+    item["director_id"] = movie[1]
+    item["adult"] = movie[2]
+    item["title"] = movie[3]
+    item["release_date"] = movie[4]
+    item["runtime"] = movie[5]
+    item["budget"] = movie[6]
+    item["revenue"] = movie[7]
+    item["imdb_id"] = movie[8]
+    item["language"] = movie[9]
+    item["overview"] = movie[10]
+    item["tagline"] = movie[11]
+    item["poster"] = movie[12]
+    item["vote_avg"] = movie[13]
+    item["vote_count"] = movie[14]
+    item["genres"] = getGenreList(id)
+    item["cast"] = getCastList(id)
+    result = {}
+    result[id] = item
 
     conn.close()
-    return {"cast": cast_list}
-    # returns {cast: {...}} or {cast: [...]}
-
-
-@app.route("/api/genres/<int:movie_id>", methods=["POST"])
-def getGenresByMovieId(movie_id):
-    genres = getGenreList(movie_id)
-    return {"genres": genres}
-    # returns {genres: {...}} or {genres: [...]}
+    return {"movie": result}
 
 
 ############### Login #####################
@@ -146,20 +129,20 @@ def getGenresByMovieId(movie_id):
 
 @app.route("/auth/login", methods=["POST"])
 def login():
-    response = request.get_json()
-    email = response["email"]
-    password = response["password"]
+    email = request.form.get("email")
+    password = request.form.get("password")
 
     return auth_login(email, password)
 
 
 @app.route("/auth/register", methods=["POST"])
 def register():
-    response = request.get_json()
-    email = response["email"]
-    password =  response["password"]
-    first_name =  response["first_name"]
-    last_name =  response["last_name"]
+    email = request.form.get("email")
+    password = request.form.get("password")
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    secret_question = request.form.get("secret_question")
+    secret_answer = request.form.get("secret_answer")
 
     # print(email)
     # print(password)
@@ -167,35 +150,85 @@ def register():
     # print(last_name)
 
     # if valid then return user
-    return auth_register(email, password, first_name, last_name)
+    return auth_register(
+        email, password, first_name, last_name, secret_question, secret_answer
+    )
 
-@app.route("/auth/changepass", methods=["POST"])
+
+@app.route("/auth/changepass")
 def ChangePassword():
-    response = request.get_json()
-    email = response["email"]
-    oldPassword = response["old_password"]
-    newPassword = response["new_password"]
+    oldPassword = request.form.get("old_password")
+    newPassword = request.form.get("new_password")
     # return something (maybe TRUE if sucessful, dunno however you want to do it)
-    return ({"worked": 1})
-
-    
+    return True
 
 
-############### Helper Functions #################
+@app.route("/auth/testing", methods=["POST"])
+def test():
+    question = get_secret_question(1)
+    print(f"question is {question}")
+    return {"question": question}
 
 
-def getGenreList(movie_id):
-    conn = sqlite3.connect("./movieDB.db")
-    cur = conn.cursor()
-    cur.execute(f"select genre from GENRE where movie_id = {movie_id};")
-    genres = []
-    for genre in cur.fetchall():
-        genres.append(genre[0])
-    print(genres)
-    conn.close()
+#################   Search    ##################
 
-    return genres
+
+@app.route("/api/search/byGenre", methods=["POST"])
+def searchMovieByGenre():
+    genre = request.form.get("genre")
+    return searchGenre(genre)
+
+
+@app.route("/api/search/byKeyword", methods=["POST"])
+def searchMovieByKeyword():
+    keyword = request.form.get("keyword")
+    return searchKeyword(keyword)
+
+
+@app.route("/api/search/byDirector", methods=["POST"])
+def searchMovieByDirector():
+    director = request.form.get("director")
+    return searchDirector(director)
+
+
+@app.route("/api/cast/<int:movie_id>", methods=["POST"])
+def getCastByMovieId(movie_id):
+    cast_list = getCastList(movie_id)
+    return {"cast": cast_list}
+
+
+@app.route("/api/genres/<int:movie_id>", methods=["POST"])
+def getGenresByMovieId(movie_id):
+    genres = getGenreList(movie_id)
+    return {"genres": genres}
+
+
+################    Review    ##################
+
+
+@app.route("/api/review/createReview", methods=["POST"])
+def createReviewForMovie():
+    user_id = int(request.form.get("user_id"))
+    movie_id = int(request.form.get("movie_id"))
+    comment = request.form.get("comment")
+    score = int(request.form.get("score"))
+    return newReview(user_id, movie_id, comment, score)
+
+
+@app.route("/api/review/likeReviewComment", methods=["POST"])
+def likeReviewComment():
+    review_id = int(request.form.get("review_id"))
+    return incrementLikes(review_id)
+
+
+@app.route("/api/review/editMovieReview", methods=["POST"])
+def editMovieReview():
+    review_id = int(request.form.get("review_id"))
+    comment = request.form.get("comment")
+    score = int(request.form.get("score"))
+    return editReview(review_id, comment, score)
 
 
 if __name__ == "__main__":
     app.run(port=5000)
+
