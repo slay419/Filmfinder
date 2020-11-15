0f29bf467a22feec7084b9ae0a9594e95df637d2
Peter Phanouvong <peterphanouvong@Peters-iMac.local>
2020-11-15 23:30:31 +1100

    an updated footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="footer">this is the footer</div>;
+  return (
+    <div className="footer">
+      <div className="left">
+        <h4>FilmFinder</h4>
+        <p>A website to find films</p>
+      </div>
+      <div className="middle">
+        <h4>COMP3900</h4>
+      </div>
+      <div className="right">
+        <h4>Developed by:</h4>
+        <ul>
+          <li>Peter</li>
+          <li>Aidan</li>
+          <li>Kevin</li>
+          <li>Steven</li>
+          <li>Michael</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
